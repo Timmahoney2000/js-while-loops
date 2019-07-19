626e4598869755fc69224213d56f5fb7be9795df
Tim <38438323+Timmahoney2000@users.noreply.github.com>
2019-07-19 12:56:51 -0400

    Add files via upload

---
+++ fishsticks.js
@@ -0,0 +1,7 @@
+alert("CONNECTED!");
+var answer = prompt("do you like fish sticks?");
+
+while(answer !== "yes") {
+    var answer = prompt("do you like fish sticks?");
+}
+alert("then you must be a gay fish");
